5e4148fb086a19b9b3d66081ac6d5203c8dbede6
ozermuharrem <muharrem.o@outlook.com>
2021-11-29 03:31:14 +0300

    js commit

---
+++ test.js
@@ -0,0 +1,17 @@
+alert("Merhaba!");
+console.log("Muharrem");
+console.log(1234);
+console.log(true);
+console.error("Bir Hata Oluştu!");
+console.warn("Uyarı vermek için");
+
+//tek satırlık yorum satırı
+
+/*
+çok satırlı yorum satırı 
+*/ 
+console.log("Consol crom tarayıcısının otomatik js derleyicisi bulunmaktadır. \n burda yazılasn kodları kullanıcı görmeden test ve kontrol debilmek için kullanılır.")
+
+ // console.clear(); bu kod ile tüm konsol ekranı temizlenir. 
+
+
